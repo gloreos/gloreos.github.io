02d6704bae3e93bd09edd60c4f8377deb771bd47
gloreos <choi.gloria99@gmail.com>
2019-05-28 13:42:33 -0700

    Changed homepage layout and added carousel to project.html

--- carousel.js
+++ carousel.js
@@ -1,28 +1,9 @@
-const carousel = document.querySelector("[data-target='carousel']");
-const card = carousel.querySelector("[data-target='card']");
-const leftButton = document.querySelector("[data-action='slideLeft']");
-const rightButton = document.querySelector("[data-action='slideRight']");
+$("#myCarousel, #myCarousel2").carousel();
 
-const carouselWidth = carousel.offsetWidth;
-const cardStyle = card.currentStyle || window.getComputedStyle(card);
-const cardMarginRight = Number(cardStyle.marginRight.match(/\d+/g)[0]);
-
-const cardCount = carousel.querySelectorAll("[data-target='card']").length;
-
-let offset = 0;
-const maxX = -((cardCount / 2) * carouselWidth +
-               (cardMarginRight * (cardCount / 2)) -
-               carouselWidth - cardMarginRight);
-
-leftButton.addEventListener("click", function() {
- if (offset !== 0) {
-   offset += carouselWidth + cardMarginRight;
-   carousel.style.transform = `translateX(${offset}px)`;
-   }
+$(".carousel-control-prev").click(function(){
+  $("#myCarousel").carousel("prev");
 });
-rightButton.addEventListener("click", function() {
- if (offset !== maxX) {
-   offset -= carouselWidth + cardMarginRight;
-   carousel.style.transform = `translateX(${offset}px)`;
- }
+
+$(".carousel-control-next").click(function(){
+  $("#myCarousel").carousel("next");
 });
