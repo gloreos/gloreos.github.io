52ab4929e78ef5493afaa2780ef6abca2f9283b1
gloreos <choi.gloria99@gmail.com>
2019-05-29 07:38:50 -0700

    Fixed carousel

--- carousel.js
+++ carousel.js
@@ -1,9 +1,9 @@
-$("#myCarousel, #myCarousel2").carousel();
+$("#myCarousel, #myCarousel2", "#myCarousel3").carousel();
 
 $(".carousel-control-prev").click(function(){
-  $("#myCarousel").carousel("prev");
+  $("#myCarousel, #myCarousel2", "#myCarousel3").carousel("prev");
 });
 
 $(".carousel-control-next").click(function(){
-  $("#myCarousel").carousel("next");
+  $("#myCarousel, #myCarousel2", "#myCarousel3").carousel("next");
 });
