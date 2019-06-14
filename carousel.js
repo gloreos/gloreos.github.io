505ca8fc3dcc470d824a5645c3f3430c6f6f32a4
Gloria Choi <gloriachoi@Glorias-MacBook-Air.local>
2019-06-13 23:17:11 -0700

    Other Writings page restructuring

--- carousel.js
+++
@@ -1,9 +0,0 @@
-$("#myCarousel, #myCarousel2", "#myCarousel3", "#myCarousel4").carousel();
-
-$(".carousel-control-prev").click(function(){
-  $("#myCarousel, #myCarousel2", "#myCarousel3", "#myCarousel4").carousel("prev");
-});
-
-$(".carousel-control-next").click(function(){
-  $("#myCarousel, #myCarousel2", "#myCarousel3", "#myCarousel4").carousel("next");
-});
